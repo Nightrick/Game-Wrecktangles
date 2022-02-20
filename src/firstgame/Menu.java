8b60f2c10d9f5902d0a6548e19680cda244b455d
Dj <Dj@Mercer>
2022-02-20 16:55:42 -0500

    This is an arcade-style game I'm working on. I intend to finish optimizing layout, level design, and overall game structure over the coming days.

---
+++ src/firstgame/Menu.java
@@ -0,0 +1,150 @@
+
+package firstgame;
+
+import firstgame.FirstGame.STATE;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+
+public class Menu extends MouseAdapter {
+    
+     FirstGame game;
+     Handler handler;
+     private HUD hud;
+     Random r = new Random();
+    
+    public Menu(FirstGame game, Handler handler, HUD hud){
+        this.game = game;
+        this.hud = hud;
+        this.handler = handler;
+    }
+    
+    public void mousePressed(MouseEvent e){
+        int mx = e.getX();
+        int my = e.getY();
+        
+        if(game.gameState == FirstGame.STATE.Menu){
+               
+        //play button
+        if(mouseOver(mx, my, 210, 150, 200, 64)){
+        game.gameState = FirstGame.STATE.Game; 
+        handler.object.clear();
+        handler.addObject(new Player(FirstGame.WIDTH/2-32, FirstGame.HEIGHT/2-32, ID.Player, handler));
+        
+        for(int i = 0; i < 1; i++)
+        handler.addObject(new BasicEnemy(r.nextInt(FirstGame.WIDTH), r.nextInt(FirstGame.HEIGHT), ID.BasicEnemy, handler));                 
+        }
+        
+        //help button
+        if(mouseOver(mx, my, 210, 250, 200, 64)){
+             game.gameState = FirstGame.STATE.Help;
+            
+        }        
+        }
+        
+        //Back Button for Help
+        if(game.gameState == FirstGame.STATE.Help){
+            if(mouseOver(mx, my, 210, 350, 200, 64)){
+                game.gameState = FirstGame.STATE.Menu;
+                return;
+            }
+        
+        }
+      
+        //Try Again Button for Game Over
+        if(game.gameState == FirstGame.STATE.End){
+            if(mouseOver(mx, my, 210, 350, 200, 64)){
+                game.gameState = FirstGame.STATE.Menu;
+                return;
+            }
+        
+    }
+        
+        //Quit Button
+        if(mouseOver(mx, my, 210, 350, 200, 64)){
+             System.exit(1);
+        }
+        
+        
+    }
+
+    
+    public void mouseReleased(MouseEvent e){
+        
+    }
+    
+    private boolean mouseOver(int mx, int my, int x, int y, int width, int height){
+        if(mx > x && mx < x + width){
+            if(my > y && my < y + height){
+                return true;
+            }else return false; 
+        }else return false;
+    }
+    
+    public void tick(){
+        
+    }
+    
+    public void render(Graphics g){
+        if(game.gameState == FirstGame.STATE.Menu){
+        Font fnt = new Font("arial", 1, 50); //Font name, style, size
+        Font fnt2 = new Font("arial", 1, 30);
+        
+        g.setFont(fnt);
+        g.setColor(Color.white);
+        g.drawString("WRECKTANGLES", 115, 70);
+        
+        g.setFont(fnt2); 
+        g.drawRect(210, 150, 200, 64);
+        g.drawString("Play", 280, 192);
+                
+        g.drawRect(210, 250, 200, 64);
+        g.drawString("Help", 280, 292);
+              
+        g.drawRect(210, 350, 200, 64);
+        g.drawString("Quit", 280, 392);
+    } else if(game.gameState == FirstGame.STATE.Help){
+        Font fnt = new Font("arial", 1, 50); //Font name, style, size
+        Font fnt2 = new Font("arial", 1, 30);
+        Font fnt3 = new Font("arial", 1, 20);
+        
+        g.setFont(fnt);
+        g.setColor(Color.white);
+        g.drawString("Help", 240, 70);
+        
+        //Text located in the help menu
+        g.setFont(fnt3);
+        g.drawString("Use arrow keys to move player and dodge enemies.", 60, 200);
+        
+        g.setFont(fnt2);
+        g.drawRect(210, 350, 200, 64);
+        g.drawString("Back", 280, 392);
+        
+    }else if(game.gameState == FirstGame.STATE.End){
+        Font fnt = new Font("arial", 1, 50); //Font name, style, size
+        Font fnt2 = new Font("arial", 1, 30);
+        Font fnt3 = new Font("arial", 1, 20);
+        
+        g.setFont(fnt);
+        g.setColor(Color.white);
+        g.drawString("GAME OVER", 170, 70);
+        
+        //Text located at the end screen
+        g.setFont(fnt3);
+        g.drawString("You died with a score of: " + hud.getScore(), 170, 200);
+        
+        g.setFont(fnt2);
+        g.drawRect(210, 350, 200, 64);
+        g.drawString("Main Menu", 235, 392);
+        
+    }
+        
+    }
+}
+    
+
+
